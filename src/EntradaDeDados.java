23c2c3b83fce7371ec6c0618d084ae258a536768
MarcosOliveiraJ <marcosoliveiradejesusjunior@gmail.com>
2026-02-03 08:02:27 -0300

    if e else imc

--- src/EntradaDeDados.java
+++ src/EntradaDeDados.java
@@ -1,22 +1,41 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class EntradaDeDados {
-    static void main(){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite vosso nome: ");
+        System.out.print("Digite vosso nome: ");
         String nome = sc.next();
-        System.out.println("Digite vossa idade: ");
+        System.out.print("Digite vossa idade: ");
         int idade = sc.nextInt();
-        System.out.printf("Digite vosso peso: ");
+        System.out.print("Digite vosso peso: ");
         double peso = sc.nextDouble();
-        System.out.printf("Digite vossa altura: ");
+        System.out.print("Digite vossa altura: ");
         double altura = sc.nextDouble();
 
-        System.out.println("Seu nome: "+nome);
-        System.out.println("Sua idade: "+idade);
-        System.out.println("Seu peso: "+peso);
-        System.out.println("Sua altura: "+altura);
-        System.out.printf("IMC: "+(peso/(altura*altura)));
+        double imc = peso / (altura * altura);
+
+        System.out.println("\n resultados");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.printf("IMC: %.2f\n", imc);
+
+
+        if (imc < 18.5) {
+            System.out.println("Classificação: Abaixo do peso");
+        } else if (imc >= 18.5 && imc < 25) {
+            System.out.println("Classificação: Peso normal (Ideal)");
+        } else if (imc >= 25 && imc < 30) {
+            System.out.println("Classificação: Sobrepeso");
+        } else if (imc >= 30 && imc < 35) {
+            System.out.println("Classificação: Obesidade Grau I");
+        } else if (imc >= 35 && imc < 40) {
+            System.out.println("Classificação: Obesidade Grau II");
+        } else {
+            System.out.println("Classificação: Obesidade Grau III (Mórbida)");
+        }
+
+        sc.close();
     }
 }
