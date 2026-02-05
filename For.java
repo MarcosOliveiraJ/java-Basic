4f78ad0eefb0fe18c803583fb05c752351342147
MarcosSidm <marcosoliveiradejesusjunior@gmail.com>
2026-02-05 09:11:27 -0300

    Add files via upload

---
+++ For.java
@@ -0,0 +1,15 @@
+package aula4;
+
+public class For {
+    static void main() {
+
+        for (int i = 0; i <= 10; i++) {
+            IO.println("valor i: " + i++);
+
+        }
+        for (int i = 1; i < 10; i++) {
+            IO.println("valor i: "+ i++);
+
+        }
+    }
+}
